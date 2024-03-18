dfe8688ceeea1645ef76cb59098788bab8bb98ec
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-03-18 11:06:07 -0500

    Orbit Report Complete

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,23 @@
+import satData from "./satData";
+import './styling.css';
+
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  console.log(displaySats);
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="flex-container">
+    
+    {displaySats.map((sat, id) => {
+      console.log(displaySats)
+      return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+
+      <button onClick={() => setSat(satData, setSat)}>All Orbits</button>
+
     </div>
   );
 };
